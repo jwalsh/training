2e163a7181b55b407954d1fe427a67c5cba23ad1
Jason Walsh <j@wal.sh>
2016-10-01 15:39:42 -0400

    map example

--- w1/fp.js
+++ w1/fp.js
@@ -1,1 +1,21 @@
 // https://danmartensen.svbtle.com/javascripts-map-reduce-and-filter
+
+var a = [1,2,3,4,5];
+
+// map()
+var double = function(n) {
+  return n * 2;
+};
+
+var b =[]
+// for (var i = 0; i < a.length; i++) {
+//   b.push(double(a[i]));
+// }
+console.log(b);
+
+var c = [];
+// var c = a.map(function(e, i, c) {
+//   return double(e);
+// });
+
+console.log(c);
